a2635eac353e89e46647698593fec11e990a442c
dengbanzhan <1336448191@qq.com>
2018-12-28 08:11:15 +0800

    12.28：更新xuelilogin

--- 张玉/xuelilogin/src/com/webtest/demo/Search.java
+++ 张玉/xuelilogin/src/com/webtest/demo/Search.java
@@ -19,7 +19,7 @@ public class Search extends BaseTest {
 		action=new Login_Action(webtest);
 	}
 	
-	@Test
+	@Test(description="完全吻合的搜索")
 	public void search_success() throws InterruptedException {
 		webtest.open("http://10.7.10.7/");
 		action.login("1336448191@qq.com", "ZHANGyu123");
@@ -32,22 +32,21 @@ public class Search extends BaseTest {
 		Assert.assertEquals(str, "大学英语","搜索结果不对");
 	}
 	
-	@Test
+	@Test(description="拼音全称搜索")
 	public void search_fail() throws InterruptedException {
 		webtest.open("http://10.7.10.7/");
 		action.login("1336448191@qq.com", "ZHANGyu123");
 		
-		webtest.type("xpath=.//*[@id='q']", "dagareharbhav");
+		webtest.type("xpath=.//*[@id='q']", "daxueyingyu");
 		webtest.click("xpath=.//*[@id='search-button']");
 		
 		Thread.sleep(3000);
 		
 		String str=webtest.panduan2("xpath=.//*[@id='id_search_tab']/div[3]/section/div[1]/div/div/div[1]/h4/a[2]");
-		Assert.assertEquals(str, "大学英语","搜索结果不对");
-		
+		Assert.assertEquals(str, "大学英语","搜索结果不对");		
 	}
 	
-	@Test
+	@Test(description="空搜索")
 	public void search_null() {
 		webtest.open("http://10.7.10.7/");
 		action.login("1336448191@qq.com", "ZHANGyu123");
@@ -55,7 +54,7 @@ public class Search extends BaseTest {
 		webtest.click("xpath=.//*[@id='search-button']");
 	}
 	
-	@Test
+	@Test(description="关键字搜索")
 	public void search_incomplete() throws InterruptedException {
 		webtest.open("http://10.7.10.7/");
 		action.login("1336448191@qq.com", "ZHANGyu123");
@@ -64,7 +63,7 @@ public class Search extends BaseTest {
 		webtest.click("xpath=.//*[@id='search-button']");
 		Thread.sleep(3000);
 		
-		boolean str=webtest.panduan3("xpath=.//*[@id='id_search_tab']/div[3]/section/div[1]/div/div/div[1]/h4/a[2]");
+		boolean str=webtest.panduan3("xpath=.//*[@id='id_search_tab']/div[3]/section/div[1]/div/div/div[1]/h4/a[2]","大学");
 		Assert.assertEquals(str, true,"搜索结果不对");
 	}
 	
